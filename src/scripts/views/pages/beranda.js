008214befecb5713ecaa2514d7ffec54b4cda070
milyasarmans <milyasarmans13@gmail.com>
2022-05-22 20:03:53 +0700

    Create Login Page

--- src/scripts/views/pages/beranda.js
+++ src/scripts/views/pages/beranda.js
@@ -3,7 +3,7 @@ import pageRender from '../../utils/page-render';
 const BerandaPage = {
   async render() {
     pageRender('beranda', 'app');
-    return '<h1>Halaman Beranda</h1>';
+    return '<h1>Halaman Beranda <i class="fa-solid fa-arrow-down"></i></h1>';
   },
 
   async afterRender() {

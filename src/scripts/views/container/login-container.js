963a0126c44a83c80519385e3666c30a87902a6e
milyasarmans <milyasarmans13@gmail.com>
2022-05-30 14:15:47 +0700

    Updated Login Page

--- src/scripts/views/container/login-container.js
+++ src/scripts/views/container/login-container.js
@@ -8,22 +8,21 @@ class LoginContainer extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="main-login">
-        <div class="left">
-          <h1>Log in to your Account</h1>
-          <img alt="fincord" src="fincord.png"/>
-          <h2>FINCORD<h2/>
-        </div>
-        <div class="right">              
-            <form action="">
-              <input type="text" id="username" name="username" placeholder="Username" required>
+          <div class="left">
+            <img alt="fincord" src="fincord.png"/>
+            <h2>FINCORD<h2/>
+          </div>
+          <div class="right">       
+              <form action="">    
+                <div class="title"><h1>Masuk<h1/></div>
+                <input type="text" id="username" name="username" placeholder="Username" required>
 
-              <input type="password" id="password" name="password" placeholder="Password" required>
-              
-              <input type="submit" value="Log in">
-              <p>Don't have an account? <a href="/#/registration">Create one</a></p>
-            </form>
-          
-        </div>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+                
+                <input type="submit" value="Masuk">
+                <p>Belum memiliki akun? <a href="/#/registration">Daftar</a></p>
+              </form>          
+          </div>
       </div>
     `;
   }

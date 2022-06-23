958042a6d3acb34e92f02e525cad2ad34f438a0a
rahmat soleh <solehr619@gmail.com>
2022-06-23 22:17:17 +0700

    Insert image

--- src/scripts/views/component/transaktion-dashboard.js
+++ src/scripts/views/component/transaktion-dashboard.js
@@ -2,33 +2,6 @@ import '../../../styles/component/transaktion-dashboard.scss';
 import cardExpends from '../items/card-transaktion';
 import progressIndicator from '../../utils/progress-bar';
 
-const transaksi = [
-  {
-    id: '111',
-    name: 'Transportasi',
-    max: 1000000,
-    use: 350000,
-  },
-  {
-    id: '222',
-    name: 'Makan dan Minum',
-    max: 900000,
-    use: 635000,
-  },
-  {
-    id: '333',
-    name: 'Hiburan',
-    max: 500000,
-    use: 450000,
-  },
-  {
-    id: '444',
-    name: 'Investasi',
-    max: 2000000,
-    use: 350000,
-  },
-];
-
 class TransaktionDashboard extends HTMLElement {
   constructor() {
     super();
@@ -38,7 +11,7 @@ class TransaktionDashboard extends HTMLElement {
   set props(value) {
     this._transaksi = value;
     const listTransaktion = document.querySelector('.dashboard-expend div');
-    listTransaktion.innerHTML = cardExpends(this._transaksi);
+    // listTransaktion.innerHTML = cardExpends(this._transaksi);
     this.renderProgressBar();
   }
 
@@ -50,7 +23,9 @@ class TransaktionDashboard extends HTMLElement {
     this.innerHTML = `
       <div class="dashboard-expend">
         <p class="title">Pengeluaran bulan ini</p>
-        <div>
+        <div class="content">
+          <img src="no-data.svg" alt="Data masih kosong"/>
+          <p>Data masih kosong</p>
         </div>
       </div>
     `;
